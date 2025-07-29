6fce2ccb17ee509b309306abb94784597b6cd9bc
kawsar0277 <kawsar1252005@gmail.com>
2025-07-29 16:41:52 +0600

    class-45 is added in repository

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>This is our fast php classd</h1>
+    
+    <table>
+    
+
+</body>
+</html>
+
+<?php
+$x = 1140;
+$y = 2120;
+
+echo $x*$y."<br>";
+
+$number1 = 156;
+$number2 = 558;
+
+$result = $number1+$number2;
+
+echo "the result is = ".$result;
+
+for($i=1; $i<=12000; $i++){
+    echo "Hello World <br>";
+}
+?>
